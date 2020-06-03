a45aa86eee76ce68ccc514821ed086a5cc400131
Anthony Faubert <anthonydfaubert@gmail.com>
2020-06-02 19:17:52 -0700

    Added CRC16 to the transmitter. Also added the ability to input a message to the UART to transmit instead of the default.

--- txmitter/txmitter.ino
+++ txmitter/txmitter.ino
@@ -1,4 +1,6 @@
 
+#include <util/crc16.h>
+
 #define SYNC_PATTERN_LEN 11
 uint8_t PATTERN_SYNC[SYNC_PATTERN_LEN] = {1,1,1,0,  0,0,1, 0,0,1, 0};
 #define BIT_PATTERN_LEN 6
@@ -10,8 +12,9 @@ uint8_t PATTERN_BIT1[BIT_PATTERN_LEN] = {0,1,1,1,1,0};
   Serial.print(num, DEC); \
   Serial.println(suffix);
 
-uint8_t* msg = (uint8_t*) "Hello, world!";
-#define MSG_LEN 13
+#define DEFAULT_MSG "Hello, world!"
+uint8_t msg[256];
+uint8_t msgLen = 13;
 int syncPattern = 0;
 int bitPattern = 0;
 uint16_t datai = 0;
@@ -19,29 +22,42 @@ uint16_t datai = 0;
 void debugVal(uint8_t val) {
   digitalWrite(3, (val & 0x01) ? HIGH : LOW);
   digitalWrite(4, (val & 0x02) ? HIGH : LOW);
-}    
+}
+
+void setMsg(char* str) {
+  uint16_t crc = 0;
+  msgLen = 0;
+  for (uint8_t i = 0; str[i] != 0; i++) {
+    crc = _crc16_update(crc, str[i]);
+    msg[i] = str[i];
+    msgLen++;
+  }
+  msg[msgLen++] = (uint8_t) ((crc & 0xFF00) >> 8);
+  msg[msgLen++] = (uint8_t) (crc & 0x00FF);
+}
 
 void setup() {
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
 
+  setMsg(DEFAULT_MSG);
+  
   Serial.begin(9600);
-  Serial.println("CSE 490W Final Project transmitter v51. Msg on next line.");
-  Serial.println((char*) msg);
-}
-
-int calls = 0;
-void delay1ms() {
-  static unsigned long clock = 0;
-  clock = micros() + 1000UL;
-  while (micros() < clock);
-  calls++;
+  Serial.println("CSE 490W Final Project transmitter v61. Msg on next line.");
+  // Println(msg):
+  for (uint8_t i = 0; i < msgLen; i++) Serial.print(msg[i]);
+  Serial.println();
 }
 
 void loop() {
   static int debug = 0;
   static unsigned long t1 = 0, t2 = 0, t3 = 0, t4 = 0;
+  static int loops = 0;
+
+  static char nextMsg[256];
+  static uint8_t nextMsgLen = 0;
+  unsigned long timeout = micros() + 1000UL;
   
   if (syncPattern < SYNC_PATTERN_LEN) { // Do sync pattern
     if (debug == 0) {
@@ -53,7 +69,7 @@ void loop() {
       _PRINTLN_SDS("Started data @", t2, "ms");
       _PRINTLN_SDS("Finished data @", t3, "ms");
       _PRINTLN_SDS("Started next sync @", t4, "ms");
-      _PRINTLN_SDS("Called delay1ms() ", calls, " times");
+      _PRINTLN_SDS("Looped ", loops, " times");
       debug++;
     }
     debugVal(1);
@@ -83,7 +99,7 @@ void loop() {
       bitPattern = 0;
       datai++;
     }
-    if (datai >= (MSG_LEN << 3)) { // if datai > bytes2bits(MSG_LEN)
+    if (datai >= ((uint16_t)msgLen << 3)) { // if datai > bytes2bits(MSG_LEN)
       if (debug == 2) {
 	t3 = millis();
 	debug++;
@@ -93,6 +109,19 @@ void loop() {
       syncPattern = 0;
     }
   }
-  
-  delay1ms();
+
+  while (micros() < timeout) { // delay for 1ms from start of function
+    if (Serial.available() > 0) { // handle input
+      char c = Serial.read();
+      if (c == '\n') {
+	nextMsg[nextMsgLen] = 0;
+	setMsg((char*) nextMsg);
+	nextMsgLen = 0;
+	if (Serial.availableForWrite() > 4) Serial.println('ACK');
+      } else {
+	nextMsg[nextMsgLen++] = (uint8_t) c;
+      }
+    }
+  }
+  loops++;
 }
